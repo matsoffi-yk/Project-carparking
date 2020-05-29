85f9fdf1210334eb843c344248fb32de1c532012
unknown <sopee_namyen@hotmail.com>
2020-05-29 17:51:50 +0700

    list getRealtime

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,186 +1,276 @@
-import React, { PureComponent, useEffect, useState } from 'react';
-import { Statistic, Col, Button } from 'antd';
-import { ArrowUpOutlined, ArrowDownOutlined, CaretUpOutlined } from '@ant-design/icons';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, PieChart, Pie, Sector, Cell, } from 'recharts';
-import car1 from '../../assets/img/card/car1.png'
-import delivery1 from '../../assets/img/card/delivery1.png'
-import parking1 from '../../assets/img/card/parking1.png'
-import vip1 from '../../assets/img/card/vip1.png'
-import car2 from '../../assets/img/card/car2.png'
-import delivery2 from '../../assets/img/card/delivery2.png'
-import parking2 from '../../assets/img/card/parking2.png'
-import vip2 from '../../assets/img/card/vip2.png'
-import price from '../../assets/img/icon/price.png'
-import car from '../../assets/img/icon/car.png'
-import parking from '../../assets/img/icon/parking.png'
+import React, { useEffect, useState } from "react";
+import { Statistic, Col } from "antd";
+import { Line as LineA } from "react-chartjs-2";
+import { LineChart, Line, CartesianGrid, PieChart, Pie, Cell, RadialBarChart, RadialBar, Legend, } from "recharts";
+import car1 from "../../assets/img/card/car1.png";
+import delivery1 from "../../assets/img/card/delivery1.png";
+import parking1 from "../../assets/img/card/parking1.png";
+import vip1 from "../../assets/img/card/vip1.png";
+import price from "../../assets/img/icon/price.png";
+import car from "../../assets/img/icon/car.png";
+import parking from "../../assets/img/icon/parking.png";
+import { Card, CardBody, Row } from "reactstrap";
+import "./card.css";
+import "./chart.css";
+import axios from "axios";
 import {
-  Card,
-  CardBody,
-  Row,
-  CardColumns
-} from 'reactstrap';
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import './card.css';
-import './chart.css';
-import axios from 'axios'
-var moment = require('moment'); // require
-const time = moment().format('DD MMMM YYYY');
+  CardActions,
+  CardHeader,
+  CardContent,
+  Button,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tooltip,
+  TableSortLabel
+} from '@material-ui/core';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+// import { StatusBullet } from 'components';
+
+
+var moment = require("moment"); // require
+const time = moment().format("DD MMMM YYYY");
 //statisce
-
-
+const data1 = [
+  { name: "A", number: 1, time: 10.1 },
+  { name: "B", number: 1.2, time: 10.3 },
+  { name: "C", number: 1.3, time: 10.6 },
+  { name: "D", number: 3, time: 10.5 },
+];
 
 //realtime
 const data2 = [
-  { name: 'Group A', value: 45 },
-  { name: 'Group B', value: 300 },
+  { name: "Group A", value: 45 },
+  { name: "Group B", value: 300 },
 ];
-const COLORS = ['#0088FE', '#00C49F'];
+
+const data3 = [
+  { name: "car", uv: 31.47, pv: 2400, fill: "#20A8D8" },
+  { name: "Car parking", uv: 26.69, pv: 4567, fill: "#FFC107" },
+  { name: "Car VIP", uv: 15.69, pv: 1398, fill: "#F86C6B" },
+];
+const style = { top: 15, left: 250, lineHeight: "24px", };
+const COLORS = ["#63C2DE", "#20A8D8"];
+
+const data4 = {
+  labelsX: ["10.00", "10.30", "11.30", "12.00", "12.02", "12.20"],
+  datasets: [
+    {
+      label: "Incoming",
+      data: [33, 53, 85, 41, 44, 65],// [x1,x2,x3,x4]
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)",
+    },
+    {
+      label: "Outgoing",
+      data: [33, 25, 35, 51, 54, 76],// [x1,x2,x3,x4]
+      fill: false,
+      borderColor: "#742774",
+    },
+  ],
+};
+
 const Dashboard = () => {
-  const [cars, setCars] = useState([{}])
-  const [dashboard, setDashboard] = useState([{}])
-  const [realtime, setRealtime] = useState([{}])
+  const [dashboard, setDashboard] = useState([{}]);
+  const [realtime, setRealtime] = useState([]);
+  const [data, setData] = useState([]);
   const [graph, setGraph] = useState([{}])
 
-  const getData = async () => {
-    const data = await axios.get(`http://127.0.0.1:8000/dashboard/getData`)
-    setCars(data.data)
-    const dash = await axios.get(`http://127.0.0.1:8000/dashboard/getDashboard`)
-    setDashboard(dash.data)
-    const realtime = await axios.get(`http://127.0.0.1:8000/dashboard/getRealtime`)
-    setRealtime(realtime.data)
-    const graph = await axios.get(`http://127.0.0.1:8000/dashboard/getGraph`)
-    setGraph(graph.data)
-  }
-
   useEffect(() => {
-    getData()
-  }, [])
-  const data = []
+    getDashboard();
+  }, []);
+
+  const getDashboard = async () => {
+    let dashboard = await axios.get("http://localhost:8000/dashboard/getDashboard");
+    setDashboard(dashboard.data);
+    let realtime = await axios.get("http://localhost:8000/dashboard/getRealtime");
+    setRealtime(realtime.data);
+    let data = await axios.get("http://localhost:8000/dashboard/getData");
+    setData(data.data);
+    let graph = await axios.get("http://localhost:8000/dashboard/getGraph")
+    setGraph(graph.data)
+  };
+  console.log("dashboard", dashboard);
+  console.log("realtime", realtime);
+  console.log("data", data);
+  console.log("graph", graph);
+
+  const datagraph = []
+  console.log("test2", datagraph);
+
   graph.forEach(deta => {
     const newData = {}
     newData.name = deta.date;
     newData.number = deta.totalCars
-    data.push(newData)
+    datagraph.push(newData)
+    console.log("test1", newData);
+
   })
 
-  console.log('cars: ', cars)
-  console.log('dashboard: ', moment(dashboard[0].newDate).format()) //ดึงเวลาให้ใช้แบบนี้
-  console.log('realtime: ', realtime)
-  console.log('graph: ', graph)
 
-
+  const renderRealtime = () => {
+    realtime.sort((a, b) => new moment(a.time) - new moment(b.time))
+    return realtime.map((v) => {
+      return (
+        <div>
+          <Row className="row">
+            <Col>{v.id}</Col>
+            <Col>{v.numberOfcars}</Col>
+            <Col>{moment(v.time).format('hh:mm:ss')}</Col>
+          </Row>
+        </div>
+      )
+    })
+  }
   return (
     <div className="animated fadeIn">
-
       {/* //การ์ด */}
       <Row>
-
-        <Col xs="12" sm="6" lg="4" md="6" >
+        <Col xs="12" sm="6" lg="2" md="6" xl="4">
           <Card className="text-white bg-info" id="card">
-            <img src={car2} className="absolute" />
+            <img src={car1} className="absolute" />
             <CardBody className="pb-0">
               <img src={car1} className="logo1" />
-
               <span className="textcard1">Number of cars</span>
             </CardBody>
-            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
               <h4 className="number">{dashboard[0].totalCars}</h4>
             </div>
           </Card>
         </Col>
-
-
-
-        <Col xs="12" sm="6" lg="4" md="6">
+        <Col xs="12" sm="6" lg="6" md="6" xl="4">
           <Card className="text-white bg-primary" id="card">
-            <img src={parking2} className="absolute" />
+            <img src={parking1} className="absolute" />
             <CardBody className="pb-0">
               <img src={parking1} className="logo2" />
               <span className="textcard2">Car Parking</span>
             </CardBody>
-            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
               <h4 className="number">{dashboard[0].carParking}</h4>
             </div>
           </Card>
         </Col>
-
-        <Col xs="12" sm="6" lg="4" md="6">
+        <Col xs="12" sm="6" lg="6" md="6" xl="4">
           <Card className="text-white bg-warning" id="card">
-            <img src={delivery2} className="absolute" />
+            <img src={delivery1} className="absolute" />
             <CardBody className="pb-0">
               <img src={delivery1} className="logo3" />
               <span className="textcard3">Delivery Parking</span>
             </CardBody>
-            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
               <h4 className="number">{dashboard[0].deliveryParking}</h4>
             </div>
           </Card>
         </Col>
-
-
-        <Col xs="12" sm="6" lg="4" md="6">
+        <Col xs="12" sm="6" lg="6" md="6" xl="4">
           <Card className="text-white bg-danger" id="card">
-            <img src={vip2} className="absolute" />
+            <img src={vip1} className="absolute" />
             <CardBody className="pb-0">
               <img src={vip1} className="logo4" />
-
               <span className="textcard4">Car VIP</span>
             </CardBody>
-            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
               <h4 className="number">{dashboard[0].carVIP}</h4>
             </div>
           </Card>
         </Col>
-
       </Row>
 
       {/* กราฟ */}
       <Row>
-        <Col xs="12" sm="6" lg="4" md="6" >
-          <Card id="chrat1">
-
-            <CardBody className="pb-0">
-              <h4>Statiscs</h4>
-              <Statistic value={cars.length} className="satistic" />
-              <div className="price">
-                <img src={price} className="price" />
-              </div>
-              <LineChart width={900} height={200} data={data} className="ss">
-                <Line type="monotone" dataKey="number" stroke="#8884d8" />
-                <CartesianGrid stroke="#ccc" />
-                {/* <XAxis dataKey="time" />
+        <Col>
+          <Row>
+            <Card id="chrat1">
+              <CardBody className="pb-0">
+                <h4>Statiscs</h4>
+                <Statistic value={data.length} className="satistic" />
+                <div className="price">
+                  <img src={price} className="price" />
+                  <text>{data1[1].number}</text>
+                  <text>({data1[2].number})</text>
+                </div>
+                <LineChart width={900} height={200} data={datagraph}>
+                  <Line dataKey="number" stroke="#8884d8" />
+                  <CartesianGrid stroke="#ccc" />
+                  {/* <XAxis dataKey="time" />
                     <YAxis dataKey="number" /> */}
-              </LineChart>
-            </CardBody>
-          </Card>
+                </LineChart>
+              </CardBody>
+            </Card>
+          </Row>
+          <Row>
+            <Col>
+              <Card className="chart4">
+                <CardBody className="pb-0">
+                  <div>
+                    <RadialBarChart
+                      width={500}
+                      height={300}
+                      cx={140}
+                      cy={70}
+                      innerRadius={20}
+                      outerRadius={70}
+                      barSize={10}
+                      data={data3}
+                    >
+                      <RadialBar
+                        minAngle={15}
+                        label={{ position: "insideStart", fill: "#fff" }}
+                        background
+                        clockWise
+                        dataKey="uv"
+                      />
+                      <Legend
+                        iconSize={10}
+                        width={120}
+                        height={140}
+                        layout="vertical"
+                        verticalAlign="middle"
+                        wrapperStyle={style}
+                      />
+                    </RadialBarChart>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="chart4">
+                <CardBody className="pb-0">
+                  <div className="chartline">
+                    <LineA data={data4} width={400} />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         </Col>
-
-        <Col xs="12" sm="6" lg="4" md="6" >
+        <Col>
           <Card id="chrat2">
-
             <CardBody className="pb-0">
-              <h5>Real time</h5>
-              <small>{time}</small>
+              <h3>Real time</h3>
+              <h6>{time}</h6>
               <div className="move">
-                <spen >Movement</spen>
+                <text>Movement</text>
                 <div>
                   <img src={car} className="rtcar" />
-                  <spen id="car" >{data2[0].value}</spen>
+                  <text id="car">{data2[0].value}</text>
                 </div>
                 <div>
                   <img src={parking} className="rtpark" />
-                  <spen id="car">{data2[1].value}</spen>
+                  <text id="car">{data2[1].value}</text>
                 </div>
-
-
               </div>
 
-              <PieChart width={700} height={900} >
-
+              <PieChart width={250} height={200}>
                 <Pie
                   data={data2}
                   cx={120}
-                  cy={200}
+                  cy={95}
                   innerRadius={60}
                   outerRadius={70}
                   fill="#8884d8"
@@ -191,122 +281,66 @@ const Dashboard = () => {
                     data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
                   }
                 </Pie>
-
               </PieChart>
-
             </CardBody>
+            {renderRealtime()}
+            <CardContent>
+              <PerfectScrollbar>
+                <div >
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Order Ref</TableCell>
+                        <TableCell>Customer</TableCell>
+                        <TableCell sortDirection="desc">
+                          <Tooltip
+                            enterDelay={300}
+                            title="Sort"
+                          >
+                            <TableSortLabel
+                              active
+                              direction="desc"
+                            >
+                              Date
+                      </TableSortLabel>
+                          </Tooltip>
+                        </TableCell>
+                        <TableCell>Status</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {realtime.map(realtime => (
+                        <TableRow
+                          hover
+                          key={realtime.id}
+                        >
+                          <TableCell>{realtime.numberOfcars}</TableCell>
+                          <TableCell>{realtime.numberOfcars}</TableCell>
+                          <TableCell>
+                            {moment(realtime.newDate).format('DD/MM/YYYY')}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              </PerfectScrollbar>
+            </CardContent>
+            <Divider />
+            <CardActions>
+              <Button
+                color="primary"
+                size="small"
+                variant="text"
+              >
+                View all <ArrowRightIcon />
+              </Button>
+            </CardActions>
           </Card>
         </Col>
-
-
-        <Row>
-          <Col xs="12" sm="6" lg="4" md="6" >
-            <Card >
-
-              <CardBody className="pb-0">
-                <h4>Statiscs</h4>
-                <Statistic value={1893} />
-                <div className="price">
-                  <img src={price} className="price" />
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-
-          <Col xs="12" sm="6" lg="4" md="6" >
-            <Card >
-
-              <CardBody className="pb-0">
-                <h4>Statiscs</h4>
-                <Statistic value={1893} />
-                <div className="price">
-                  <img src={price} className="price" />
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-
-
-
-        </Row>
-
-
-
-        {/* <Col xs="12" sm="6" lg="4" md="6">
-          <Card className="text-white bg-primary" id="card">
-          </Card>
-        </Col>
-        <Col>
-        <div className="animated fadeIn">
-          <CardColumns className="cols-2">
-            <Card id="frame">
-              <CardBody>
-                <h4>Statiscs</h4>
-                <Statistic value={1893} className="satistic" />
-                <div className="price">
-                  <img src={price} className="price" />
-                  <spen  >{data[5].number}</spen>
-                  <spen>({data[4].number})</spen>
-                </div>
-
-
-
-                <div className="chart-wrapper" >
-                  <LineChart width={400} height={200} data={data}>
-                    <Line type="monotone" dataKey="number" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" />
-                    {/* <XAxis dataKey="time" />
-                    <YAxis dataKey="number" /> */}
-        {/* </LineChart>
-                </div>
-              </CardBody>
-            </Card>
-          </CardColumns>
-        </div>
-        </Col> */}
-
-        {/* <Col>
-        <div className="animated fadeIn">
-          <CardColumns className="cols-2">
-            <Card id="frame">
-              <CardBody>
-                <h5>Real time</h5>
-                <small>{time}</small>
-                <PieChart width={300} height={400} >
-
-                  <Pie
-                    data={data2}
-                    cx={120}
-                    cy={200}
-                    innerRadius={60}
-                    outerRadius={70}
-                    fill="#8884d8"
-                    paddingAngle={5}
-                    dataKey="value"
-                  >
-                    {
-                      data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-                    }
-                  </Pie>
-
-                </PieChart>
-              </CardBody>
-            </Card>
-          </CardColumns>
-        </div>         
-        </Col> */}
-
-
-
       </Row>
-
-
-
-
-
     </div>
   );
-
-}
+};
 
 export default Dashboard;
