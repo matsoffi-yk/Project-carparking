709c4fb09f3232a0fa3bd03ae00c4ad6950d0e55
unknown <sopee_namyen@hotmail.com>
2020-06-02 13:14:15 +0700

    update

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Statistic, Col } from "antd";
 import { Line as LineA } from "react-chartjs-2";
-import { LineChart, Line, CartesianGrid, PieChart, Pie, Cell, RadialBarChart, RadialBar, Legend, } from "recharts";
+import {LineChart,Line,CartesianGrid,PieChart,Pie,Cell,RadialBarChart,RadialBar,Legend,} from "recharts";
 import car1 from "../../assets/img/card/car1.png";
 import delivery1 from "../../assets/img/card/delivery1.png";
 import parking1 from "../../assets/img/card/parking1.png";
@@ -13,56 +13,37 @@ import { Card, CardBody, Row } from "reactstrap";
 import "./card.css";
 import "./chart.css";
 import axios from "axios";
-import {
-  CardActions,
-  CardHeader,
-  CardContent,
-  Button,
-  Divider,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Tooltip,
-  TableSortLabel
-} from '@material-ui/core';
+import {CardActions,CardContent,Button,Divider,Table,TableBody,TableCell,TableRow,} from '@material-ui/core';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import PerfectScrollbar from 'react-perfect-scrollbar';
-// import { StatusBullet } from 'components';
 
-
-var moment = require("moment"); // require
+var moment = require("moment"); 
 const time = moment().format("DD MMMM YYYY");
 
-
-//realtime
 const data2 = [
   { name: "Group A", value: 45 },
   { name: "Group B", value: 300 },
 ];
-
 const data3 = [
-  { name: "car", uv: 31.47, pv: 2400, fill: "#20A8D8" },
-  { name: "Car parking", uv: 26.69, pv: 4567, fill: "#FFC107" },
-  { name: "Car VIP", uv: 15.69, pv: 1398, fill: "#F86C6B" },
+  { name: "car",        uv: 31.47,pv: 2400,fill: "#20A8D8"},
+  { name: "Car parking",uv: 26.69,pv: 4567,fill: "#FFC107"},
+  { name: "Car VIP",    uv: 15.69,pv: 1398,fill: "#F86C6B"},
 ];
-const style = { top: 15, left: 250, lineHeight: "24px", };
+const style = {top: 15,left: 250,lineHeight: "24px",};
 const COLORS = ["#63C2DE", "#20A8D8"];
-
 const data4 = {
-  labelsX: ["10.00", "10.30", "11.30", "12.00", "12.02", "12.20"],
+  labels: ["10.00", "10.30", "11.30", "12.00", "12.02", "12.20"],
   datasets: [
     {
       label: "Incoming",
-      data: [33, 53, 85, 41, 44, 65],// [x1,x2,x3,x4]
+      data: [33, 53, 85, 41, 44, 65],
       fill: true,
       backgroundColor: "rgba(75,192,192,0.2)",
       borderColor: "rgba(75,192,192,1)",
     },
     {
       label: "Outgoing",
-      data: [33, 25, 35, 51, 54, 76],// [x1,x2,x3,x4]
+      data: [33, 25, 35, 51, 54, 76],
       fill: false,
       borderColor: "#742774",
     },
@@ -74,11 +55,9 @@ const Dashboard = () => {
   const [realtime, setRealtime] = useState([]);
   const [data, setData] = useState([]);
   const [graph, setGraph] = useState([{}])
-
   useEffect(() => {
     getDashboard();
   }, []);
-
   const getDashboard = async () => {
     let dashboard = await axios.get("http://localhost:8000/dashboard/getDashboard");
     setDashboard(dashboard.data);
@@ -92,92 +71,24 @@ const Dashboard = () => {
   console.log("dashboard", dashboard);
   console.log("realtime", realtime);
   console.log("data", data);
-  console.log("graph", graph);
-
+  console.log("graph",graph);
   const datagraph = []
-  console.log("test2", datagraph);
-
   graph.forEach(deta => {
     const newData = {}
     newData.name = deta.date;
     newData.number = deta.totalCars
     datagraph.push(newData)
-    console.log("test1", newData);
-
+    console.log("test1",newData);
   })
-  const [task, setTask] = useState(realtime.map(realtime => (
-    <TableRow
-      hover
-      key={realtime.id}
-    >
-      <TableCell>{realtime.numberOfcars}</TableCell>
-      <TableCell>{realtime.numberOfcars}</TableCell>
-      <TableCell>
-        {moment(realtime.time).format('hh:mm:ss')}
-      </TableCell>
-    </TableRow>
-  )))
-  const viewAll = () => {
-    if (task === realtime.map(realtime => (
-      <TableRow
-        hover
-        key={realtime.id}
-      >
-        <TableCell>{realtime.numberOfcars}</TableCell>
-        <TableCell>{realtime.numberOfcars}</TableCell>
-        <TableCell>
-          {moment(realtime.time).format('hh:mm:ss')}
-        </TableCell>
-      </TableRow>
-    ))) {
-      return setTask(realtime.slice(0, 4).map(realtime => (
-        <TableRow
-          hover
-          key={realtime.id}
-        >
-          <TableCell>{realtime.numberOfcars}</TableCell>
-          <TableCell>{realtime.numberOfcars}</TableCell>
-          <TableCell>
-            {moment(realtime.time).format('hh:mm:ss')}
-          </TableCell>
-        </TableRow>
-      )))
-    } else {
-      return setTask(realtime.map(realtime => (
-        <TableRow
-          hover
-          key={realtime.id}
-        >
-          <TableCell>{realtime.numberOfcars}</TableCell>
-          <TableCell>{realtime.numberOfcars}</TableCell>
-          <TableCell>
-            {moment(realtime.time).format('hh:mm:ss')}
-          </TableCell>
-        </TableRow>
-      )))
-    }
-  }
-  console.log(realtime.slice(0, 4).map(realtime => (
-    <TableRow
-      hover
-      key={realtime.id}
-    >
-      <TableCell>{realtime.numberOfcars}</TableCell>
-      <TableCell>{realtime.numberOfcars}</TableCell>
-      <TableCell>
-        {moment(realtime.time).format('hh:mm:ss')}
-      </TableCell>
-    </TableRow>
-  )))
+
   return (
     <div className="animated fadeIn">
-      {/* //การ์ด */}
       <Row>
         <Col xs="12" sm="6" lg="2" md="6" xl="4">
-          <Card className="text-white bg-info" id="card">
-            <img src={car1} className="absolute" />
+          <Card className="text-white bg-primary" id="card">
+            <img src={car1} alt="logocar" className="absolute" />
             <CardBody className="pb-0">
-              <img src={car1} className="logo1" />
+              <img src={car1} alt="logocar" className="logo1" />
               <span className="textcard1">Number of cars</span>
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
@@ -186,10 +97,10 @@ const Dashboard = () => {
           </Card>
         </Col>
         <Col xs="12" sm="6" lg="6" md="6" xl="4">
-          <Card className="text-white bg-primary" id="card">
-            <img src={parking1} className="absolute" />
+          <Card className="text-white bg-info" id="card">
+            <img src={parking1} alt="logoparking" className="absolute" />
             <CardBody className="pb-0">
-              <img src={parking1} className="logo2" />
+              <img src={parking1} alt="logoparking" className="logo2" />
               <span className="textcard2">Car Parking</span>
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
@@ -199,9 +110,9 @@ const Dashboard = () => {
         </Col>
         <Col xs="12" sm="6" lg="6" md="6" xl="4">
           <Card className="text-white bg-warning" id="card">
-            <img src={delivery1} className="absolute" />
+            <img src={delivery1} alt="logodelivery" className="absolute" />
             <CardBody className="pb-0">
-              <img src={delivery1} className="logo3" />
+              <img src={delivery1} alt="logodelivery" className="logo3" />
               <span className="textcard3">Delivery Parking</span>
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
@@ -211,9 +122,9 @@ const Dashboard = () => {
         </Col>
         <Col xs="12" sm="6" lg="6" md="6" xl="4">
           <Card className="text-white bg-danger" id="card">
-            <img src={vip1} className="absolute" />
+            <img src={vip1} alt="logovip" className="absolute" />
             <CardBody className="pb-0">
-              <img src={vip1} className="logo4" />
+              <img src={vip1} alt="logovip" className="logo4" />
               <span className="textcard4">Car VIP</span>
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: "70px" }}>
@@ -223,7 +134,6 @@ const Dashboard = () => {
         </Col>
       </Row>
 
-      {/* กราฟ */}
       <Row>
         <Col>
           <Row>
@@ -232,13 +142,14 @@ const Dashboard = () => {
                 <h4>Statiscs</h4>
                 <Statistic value={data.length} className="satistic" />
                 <div className="price">
-                  <img src={price} className="price" />
+                  <img src={price} alt="logo" className="price" />
+                  <text>{dashboard[0].totalCars}</text>
+                  <text>({dashboard[0].totalCars})</text>
                 </div>
                 <LineChart width={900} height={200} data={datagraph}>
                   <Line dataKey="number" stroke="#8884d8" />
                   <CartesianGrid stroke="#ccc" />
-                  {/* <XAxis dataKey="time" />
-                    <YAxis dataKey="number" /> */}
+
                 </LineChart>
               </CardBody>
             </Card>
@@ -297,15 +208,15 @@ const Dashboard = () => {
               <div className="move">
                 <text>Movement</text>
                 <div>
-                  <img src={car} className="rtcar" />
-                  <text id="car">{data2[0].value}</text>
+                  <img src={car} alt="logo" className="rtcar" />
+                  <text id="car">{dashboard[0].totalCars}</text>
                 </div>
                 <div>
-                  <img src={parking} className="rtpark" />
-                  <text id="car">{data2[1].value}</text>
+                  <img src={parking} alt="logo" className="rtpark" />
+                  <text id="car">{dashboard[0].carParking}</text>
                 </div>
               </div>
-
+             
               <PieChart width={250} height={200}>
                 <Pie
                   data={data2}
@@ -322,16 +233,27 @@ const Dashboard = () => {
                   }
                 </Pie>
               </PieChart>
-            </CardBody>
+            </CardBody> 
             <CardContent>
               <PerfectScrollbar>
-                <div >
+               
                   <Table>
                     <TableBody>
-                      {task}
+                      {realtime.slice(0,3).map(realtime => (
+                        <TableRow
+                          hover
+                          key={realtime.id}
+                        >
+                          <TableCell>{realtime.numberOfcars}</TableCell>
+                          <TableCell>{realtime.numberOfcars}</TableCell>
+                          <TableCell>
+                            {moment(realtime.time).format('hh:mm:ss')}
+                          </TableCell>
+                        </TableRow>
+                      ))}
                     </TableBody>
                   </Table>
-                </div>
+               
               </PerfectScrollbar>
             </CardContent>
             <Divider />
@@ -340,7 +262,6 @@ const Dashboard = () => {
                 color="primary"
                 size="small"
                 variant="text"
-                onClick={viewAll}
               >
                 View all <ArrowRightIcon />
               </Button>
